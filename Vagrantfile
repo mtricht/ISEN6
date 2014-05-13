2e9952b5c71750883ddc3fdcf0015f1776a3b7d8
Michael <michaelvantricht@gmail.com>
2014-05-13 20:19:33 +0200

    Vagrant added, phalconphp added and .gitignore updated.

---
+++ Vagrantfile
@@ -0,0 +1,8 @@
+Vagrant.configure("2") do |config|
+  config.vm.box = "chef/centos-6.5"
+  config.vm.network :forwarded_port, guest: 80, host: 80
+  config.vm.network :forwarded_port, guest: 5000, host: 5000
+  config.vm.provision "shell", path: "./vagrant-setup.sh"
+  config.vm.synced_folder "./web", "/var/www/html/web"
+  config.vm.synced_folder "./server", "/var/www/html/server"
+end
---
+++ vagrant-setup.sh
@@ -0,0 +1,33 @@
+#! /bin/bash
+
+# Requirements
+sudo rpm -Uvh http://mirror.webtatic.com/yum/el6/latest.rpm
+sudo yum -y install vim httpd mysql mysql-server php55w php55w-opcache php55w-mysql php55w-devel gcc libtool git wget pcre-devel.x86_64
+
+# pip and flask
+sudo wget https://bootstrap.pypa.io/get-pip.py
+sudo python get-pip.py
+sudo pip install flask
+
+# Phalconphp
+sudo git clone --depth=1 git://github.com/phalcon/cphalcon.git
+cd cphalcon/build
+sudo ./install
+sudo echo 'extension=phalcon.so' > /etc/php.d/phalcon.ini
+sudo git clone git://github.com/phalcon/phalcon-devtools.git
+cd phalcon-devtools/
+sudo ./phalcon.sh
+sudo ln -s ~/phalcon-devtools/phalcon.php /usr/bin/phalcon
+sudo chmod ugo+x /usr/bin/phalcon
+
+# Start stuff
+sudo chkconfig httpd on
+sudo chkconfig mysqld on
+sudo service mysqld start
+sudo service httpd start
+
+# bitcoind
+sudo rpm -ivh http://tvdw.eu/bitcoin-0.8.1-1.el6.`uname -m`.rpm
+
+# Sometimes httpd wont start.
+# AllowOverride is enabled in /etc/httpd/conf/httpd.conf
