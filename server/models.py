4a9bc174e3cb3849f3a0db9e8326bf5cd63f29b1
Peter <peter@vaassens.nl>
2014-07-03 10:59:50 +0200

    API progress

--- server/models.py
+++ server/models.py
@@ -3,10 +3,12 @@ from flaskext.mysql import MySQL
 
 def findAccount(account_id):
 	db = g.db.cursor()
-	db.execute('SELECT * FROM `keys` WHERE `rsa_id` = %s LIMIT 1', [account_id])
+	db.execute('SELECT `card_id`, `rsa_public_key` FROM `keys` WHERE `card_id` = %s LIMIT 1', [account_id])
 	result = db.fetchone()
 	if result is None:
 		return None
+	print("hey ho wat doe je nou die tekeningen zijn niet van jou")
+	print(result[1])
 	return Account(result[0], result[1])
 
 class Account:
