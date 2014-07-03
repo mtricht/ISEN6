e6558e97835acf1dfa52210c0e263a176116bc33
Michael <michaelvantricht@gmail.com>
2014-07-03 13:40:57 +0200

    Fullscreen addded back and we now can close it.

--- terminal/Isen/src/startup/TerminalApp.java
+++ terminal/Isen/src/startup/TerminalApp.java
@@ -19,9 +19,7 @@ public class TerminalApp extends JFrame{
 		FullScreen f = new FullScreen();
 		DisplayMode dm = new DisplayMode(1366, 768, 32, DisplayMode.REFRESH_RATE_UNKNOWN);
 		
-		//f.setFullScreen(dm, this);
-		setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
-		//setIconImage();
+		f.setFullScreen(dm, this);
 
 		JPanel panel = new JPanel(new BorderLayout());
 		Screen sc = new Screen();
