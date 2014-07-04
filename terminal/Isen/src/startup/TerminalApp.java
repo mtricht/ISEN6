7c081b06a7efad9914179abd41ccdeac23ead741
Michael <michaelvantricht@gmail.com>
2014-07-04 15:07:34 +0200

    Forgot to add jars.

--- terminal/Isen/src/startup/TerminalApp.java
+++ terminal/Isen/src/startup/TerminalApp.java
@@ -17,7 +17,7 @@ public class TerminalApp extends JFrame {
 	public TerminalApp()
 	{
 		FullScreen f = new FullScreen();
-		DisplayMode dm = new DisplayMode(1366, 768, 32, DisplayMode.REFRESH_RATE_UNKNOWN);
+		DisplayMode dm = new DisplayMode(1366, 768, 16, DisplayMode.REFRESH_RATE_UNKNOWN);
 		
 		f.setFullScreen(dm, this);
 
